a0399c73e2a085ec705f735b0989eafb77a5b753
deyvissonsouza100 <deyvissonsouza100@gmail.com>
2026-03-26 15:38:04 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-  siteTitle: 'Carnaval 2027 • Painel Operacional',
-  supabaseUrl: 'https://uhmzkpckjqvfxwstyhtx.supabase.co',
-  supabaseAnonKey: 'sb_publishable_-WC4000_m1W0WwCsowa5IA_5FasvZFr',
-  requireLogin: false,
-  defaultPageSize: 15,
-  carnavalYear: 2027
-};
